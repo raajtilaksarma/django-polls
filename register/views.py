de1663805c687fa5bb31054331b1e21bdf886cdf
raajtilaksarma <raajtilaksarma@gmail.com>
2020-07-03 08:42:27 -0400

    add register link and login after register

--- register/views.py
+++ register/views.py
@@ -8,7 +8,7 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-        return redirect("/home")
+        return redirect("/login")
     else:
         form = RegisterForm()
 
